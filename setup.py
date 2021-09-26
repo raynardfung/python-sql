28dc9175c1c889d667bdd715b0687aa1c7e61c25
misterflannel <ray.fung@gmail.com>
2021-09-25 20:01:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pythonsql',
     description='Python utility tool for interacting with SQL.',
+    url='https://github.com/raynardfung/python-sql',
+    license='MIT',
     packages=find_packages()
 )

fa97800773972e29a94f6d054499ce9d928547a5
misterflannel <ray.fung@gmail.com>
2021-10-22 14:04:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='pythonsql',
     description='Python utility tool for interacting with SQL.',
     url='https://github.com/raynardfung/python-sql',
     license='MIT',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=requirements
 )
